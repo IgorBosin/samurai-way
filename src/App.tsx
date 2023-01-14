6ad3cf389c7c4c99e1f4f51127fa2766d642a245
IgorBosin <ibosin@yandex.ru>
2023-01-14 19:06:50 +0300

    lesson 1-7

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,33 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-     Hello, samurai! Let's go!
-    </div>
-  );
+    return (
+        <div>
+            <Header/>
+            <Technologies/>
+        </div>
+    );
+}
+
+function Header() {
+    return (
+        <div>
+            <a href='#s'>Home</a>
+            <a href='#s'>News Feed</a>
+            <a href='#s'>Messages</a>
+        </div>
+    )
+}
+
+function Technologies() {
+    return (
+            <ul>
+                <li>css</li>
+                <li>html</li>
+                <li>js</li>
+                <li>react</li>
+            </ul>
+    )
 }
 
 export default App;
